96ecc42c67972ab0e72b8cb89f8b949ef9652d63
Jakob Helmecke <j.helmecke@gmail.com>
2021-11-25 18:22:51 +0100

    fix(indentline): show context

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -227,6 +227,9 @@ return packer.startup(function()
       require('indent_blankline').setup {
         buftype_exclude = { 'terminal' },
         filetype_exclude = { 'help', 'packer', 'NvimTree' },
+        show_current_context = true,
+        show_current_context_start = true,
+        show_first_indent_level = false,
       }
     end,
   }
