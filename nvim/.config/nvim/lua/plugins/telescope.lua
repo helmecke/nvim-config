d5b5e4226b783ad1528145bdec621fdbbbc4094d
helmecke <j.helmecke@gmail.com>
2021-12-21 08:05:29 +0100

    fix(telescope): remove which-key dependency

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -47,30 +47,15 @@ require('git-worktree').setup {
 -- end})
 
 vim.api.nvim_set_keymap('n', '<a-g>', '<cmd>Telescope ghq list<cr>', { noremap = true })
-
-require('which-key').register {
-  ['<leader>'] = {
-    b = {
-      name = '+buffer',
-      b = { '<cmd>Telescope buffers<cr>', 'Find buffer' },
-    },
-    f = {
-      name = '+find',
-      f = { '<cmd>Telescope find_files<cr>', 'Find file' },
-      g = { '<cmd>Telescope live_grep<cr>', 'Find string' },
-      r = { '<cmd>Telescope oldfiles<cr>', 'Find recent file' },
-      m = { '<cmd>Telescope man_pages<cr>', 'Find man page' },
-    },
-    g = {
-      name = '+git',
-      r = { '<cmd>Telescope ghq list<cr>', 'Find repository' },
-      f = { '<cmd>Telescope git_files<cr>', 'Find file' },
-      c = { '<cmd>Telescope git_commits<cr>', 'Find commit' },
-      b = { '<cmd>Telescope git_branches<cr>', 'Find branch' },
-      s = { '<cmd>Telescope git_status<cr>', 'Find change' },
-      S = { '<cmd>Telescope git_stash<cr>', 'Find stash' },
-      C = { '<cmd>Telescope git_bcommits<cr>', 'Find buffer commit' },
-      w = { '<cmd>Telescope git_worktree git_worktrees<cr>', 'Find worktree' },
-    },
-  },
-}
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fr', '<cmd>Telescope oldfiles<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fm', '<cmd>Telescope man_pages<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gr', '<cmd>Telescope ghq list<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gf', '<cmd>Telescope git_files<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gc', '<cmd>Telescope git_commits<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gb', '<cmd>Telescope git_branches<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gs', '<cmd>Telescope git_status<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gS', '<cmd>Telescope git_stash<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gC', '<cmd>Telescope git_bcommits<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gw', '<cmd>Telescope git_worktree git_worktrees<cr>', { noremap = true })
