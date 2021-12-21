a1089f9f8c2a3aa24502fe0028bdd5b71581c3ef
helmecke <j.helmecke@gmail.com>
2021-12-21 08:04:52 +0100

    feat(gitsigns): add plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -244,4 +244,12 @@ return packer.startup(function()
   use {
     'towolf/vim-helm',
   }
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      -- ./gitsigns.lua
+      require 'plugins.gitsigns'
+    end,
+  }
 end)
