f6b1732bd638bf3e9f6adf457ca6278342ab9e35
Jakob Helmecke <j.helmecke@gmail.com>
2021-12-21 16:37:39 +0100

    fix(bufferline): match gitsigns indicator

--- nvim/.config/nvim/lua/plugins/nvim-bufferline.lua
+++ nvim/.config/nvim/lua/plugins/nvim-bufferline.lua
@@ -1,9 +1,17 @@
+local colors = require 'onedark.colors'
+
 require('bufferline').setup {
   options = {
+    indicator_icon = '▍',
     offsets = { { filetype = 'NvimTree', text = 'File Explorer', highlight = 'Directory', text_align = 'left' } },
     show_close_icon = false,
     show_buffer_close_icons = false,
     diagnostics = 'nvim_lsp',
+  },
+  highlights = {
+    indicator_selected = {
+      guifg = colors.blue.gui,
+    },
   },
 }
 
