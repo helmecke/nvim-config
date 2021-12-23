718bb40159eef31278313ec3100569b8caa8a729
Jakob Helmecke <j.helmecke@gmail.com>
2021-12-23 14:22:53 +0100

    feat(null-ls): add yamllint diagnostics

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -273,6 +273,7 @@ null_ls.setup {
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.code_actions.shellcheck,
     null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.formatting.stylua,
   },
 }
