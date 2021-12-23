550ee079ca6e4bfc3e1825d86f74c6de0f86f3e2
Jakob Helmecke <j.helmecke@gmail.com>
2021-12-23 10:47:26 +0100

    fix(helm): disable yamlls for helm templates

--- nvim/.config/nvim/lua/configs/autogroups.lua
+++ nvim/.config/nvim/lua/configs/autogroups.lua
@@ -6,6 +6,10 @@ local autogroups = {
     'TermOpen * :set nonumber norelativenumber',
     'TermOpen * nnoremap <buffer> <C-c> i<C-c>',
   },
+  helm_yamlls = {
+    'BufEnter,WinEnter *helm*/*/templates/*.{yml,yaml} :LspStop yamlls',
+    'BufLeave *helm*/*/templates/*.{yml,yaml} :LspStart yamlls',
+  },
 }
 
 utils.create_augroup(autogroups)
