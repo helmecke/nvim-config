c8e543392a9f3ca95c8dba350cf679f32dbaf1aa
helmecke <j.helmecke@gmail.com>
2022-01-14 23:05:02 +0100

    fix(treesitter): remove unused modules

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,3 @@
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
 parser_configs.norg = {
@@ -50,65 +47,5 @@ require('nvim-treesitter.configs').setup {
   indent = {
     enable = true,
     disable = { 'yaml' },
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '=',
-      node_incremental = '=',
-      node_decremental = '-',
-      scope_incremental = '+',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['aC'] = '@conditional.outer',
-        ['iC'] = '@conditional.inner',
-
-        -- Or you can define your own textobjects like this
-        ['iF'] = {
-          python = '(function_definition) @function',
-          cpp = '(function_definition) @function',
-          c = '(function_definition) @function',
-          java = '(method_declaration) @function',
-        },
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['[w'] = '@parameter.inner',
-      },
-      swap_previous = {
-        [']w'] = '@parameter.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
   },
 }
