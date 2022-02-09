d8826723cfb2b3d92729423c2f599193e3551f69
Jakob Helmecke <j.helmecke@gmail.com>
2022-02-09 12:41:26 +0100

    fix(nvim-tree): use nvim-tree instead of netrw

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -14,7 +14,6 @@ local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
 require('nvim-tree').setup {
   disable_netrw = false,
-  hijack_netrw = false,
   update_cwd = true,
   view = {
     mappings = {
