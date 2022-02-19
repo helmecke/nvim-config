3741b7286b48c341938f5eaa9f907d2b475245eb
helmecke <j.helmecke@gmail.com>
2022-02-19 10:52:38 +0100

    refactor(zsh): remove custom folds and use marker

--- nvim/.config/nvim/ftplugin/zsh.lua
+++ nvim/.config/nvim/ftplugin/zsh.lua
@@ -1,6 +1,4 @@
 vim.bo.shiftwidth = 2
 vim.bo.expandtab = true
 vim.bo.tabstop = 2
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'ZshFolds(v:lnum)'
-vim.opt.foldtext = 'ZshFoldText()'
+vim.wo.foldmethod = 'marker'
