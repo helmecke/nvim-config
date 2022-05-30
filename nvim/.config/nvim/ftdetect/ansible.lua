db2bd4196dc6b9eabccee94f7308d29a2b6edb8a
Jakob Helmecke <j.helmecke@gmail.com>
2022-05-30 08:06:44 +0200

    fix(ftdetect): ansible

--- nvim/.config/nvim/ftdetect/ansible.lua
+++ nvim/.config/nvim/ftdetect/ansible.lua
@@ -1,1 +1,2 @@
-vim.cmd 'autocmd BufRead,BufNewFile */comp/*.{yml,yaml} set filetype=yaml.ansible'
+vim.cmd 'autocmd BufRead,BufNewFile */comp/{playbook_*,group_vars/*,host_vars/*}.{yml,yaml} set filetype=yaml.ansible'
+vim.cmd 'autocmd BufRead,BufNewFile */{de.hacon.hafas.ansible.roles.*}/*.{yml,yaml} set filetype=yaml.ansible'
