adfc6a27d31841617ef4e0c46b6b9284235b7380
helmecke <j.helmecke@gmail.com>
2022-06-02 22:35:42 +0200

    feat: disable unused builtin plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,5 +4,32 @@ if present then
   impatient.enable_profile()
 end
 
+-- disable some builtin vim plugins
+local default_plugins = {
+  '2html_plugin',
+  'getscript',
+  'getscriptPlugin',
+  'gzip',
+  'logipat',
+  -- 'netrw', -- needed for fugitives :GB
+  'netrwPlugin',
+  'netrwSettings',
+  'netrwFileHandlers',
+  'matchit',
+  'tar',
+  'tarPlugin',
+  'rrhelper',
+  'spellfile_plugin',
+  'vimball',
+  'vimballPlugin',
+  'zip',
+  'zipPlugin',
+  'fzf',
+}
+
+for _, plugin in pairs(default_plugins) do
+  vim.g['loaded_' .. plugin] = 1
+end
+
 require 'configs' -- ./lua/configs/init.lua
 require 'plugins' -- ./lua/plugins/init.lua
