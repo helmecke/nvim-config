0bb2a8dd6f63dbb9b296f74d41c16a7bee90d631
Jakob Helmecke <j.helmecke@gmail.com>
2022-08-25 08:08:19 +0200

    feat(mappings): add copy to clipboard
    same mapping as in kitty

--- nvim/.config/nvim/lua/config/mappings.lua
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -46,6 +46,7 @@ vim.keymap.set('n', 'J', 'mzJ`z')
 
 -- convinient system yank or paste
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'yank to clipboard' })
+vim.keymap.set('v', '<c-C>', '"+y', { desc = 'yank to clipboard' })
 vim.keymap.set('n', '<leader>p', '"+p', { desc = 'paste from clipboard below' })
 vim.keymap.set('n', '<leader>P', '"+P', { desc = 'paste from clipboard abow' })
 vim.keymap.set('v', '<leader>p', '"+p', { desc = 'paste from clipboard' })
