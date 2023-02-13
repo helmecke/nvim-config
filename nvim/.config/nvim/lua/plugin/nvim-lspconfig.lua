34b59b0ffc708025ea89c087a3d2cbdd71d1c581
Jakob Helmecke <j.helmecke@gmail.com>
2023-02-13 12:12:10 +0100

    fix(lspconfig): replace deprecated sumneko_lua

--- nvim/.config/nvim/lua/plugin/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugin/nvim-lspconfig.lua
@@ -269,7 +269,7 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-require('lspconfig').sumneko_lua.setup {
+require('lspconfig').lua_ls.setup {
   on_init = custom_init,
   on_attach = function(client, bufnr)
     client.server_capabilities.documentFormattingProvider = false
