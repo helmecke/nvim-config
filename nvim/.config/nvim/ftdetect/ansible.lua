6a05012fc94a3cc21e0e5bf90ee86ae994988471
Jakob Helmecke <j.helmecke@gmail.com>
2023-04-16 08:48:18 +0200

    fix(ftdetect): add playbooks repo

--- nvim/.config/nvim/ftdetect/ansible.lua
+++ nvim/.config/nvim/ftdetect/ansible.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   group = group,
   pattern = {
     '*/comp/{playbook_*,group_vars/*,host_vars/*}.{yml,yaml}',
-    '*/{de.hacon.hafas.ansible.roles.*}/*.{yml,yaml}',
+    '*/{de.hacon.hafas.ansible.roles.*,de.hacon.hafas.ansible.playbooks}/*.{yml,yaml}',
   },
   command = 'setl ft=yaml.ansible',
 })
