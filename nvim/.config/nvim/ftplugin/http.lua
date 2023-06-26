6fe96dfbd2feeeb5d196686d4dd68b1f8a72f6ad
Jakob Helmecke <j.helmecke@gmail.com>
2023-06-26 17:12:18 +0200

    fix(rest-nvim): mapping conflict

--- nvim/.config/nvim/ftplugin/http.lua
+++ nvim/.config/nvim/ftplugin/http.lua
@@ -1,3 +1,2 @@
-vim.keymap.set('n', '<cr>', '<plug>RestNvim', { buffer = 0, silent = true })
-vim.keymap.set('n', '<c-cr>', '<plug>RestNvimLast', { buffer = 0, silent = true })
+vim.keymap.set('n', '<c-cr>', '<plug>RestNvim', { buffer = 0, silent = true })
 vim.keymap.set('n', '<c-p>', '<plug>RestNvimPreview', { buffer = 0, silent = true })
