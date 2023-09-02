c046fef3f0adecaaa9d359791c1cb1e5e44a14de
helmecke <j.helmecke@gmail.com>
2023-09-02 19:46:39 +0200

    chore(plugins): missing fmt

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -8,7 +8,7 @@ return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.markdownlint
+        null_ls.builtins.diagnostics.markdownlint,
       },
     }
   end,
