88a85c340176f5ca9a39e093a98123051c99b802
Jakob Helmecke <j.helmecke@gmail.com>
2023-10-04 09:57:56 +0200

    fix(ftplugin): use localleader for http mappings

--- nvim/.config/nvim/ftplugin/http.lua
+++ nvim/.config/nvim/ftplugin/http.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '<c-cr>', '<plug>RestNvim', { buffer = 0, silent = true })
-vim.keymap.set('n', '<c-p>', '<plug>RestNvimPreview', { buffer = 0, silent = true })
+vim.keymap.set("n", "<localleader>r", "<plug>RestNvim", { desc = "Run", buffer = 0, silent = true })
+vim.keymap.set("n", "<localleader>p", "<plug>RestNvimPreview", { desc = "Preview", buffer = 0, silent = true })
